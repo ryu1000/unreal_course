5c34ddd1de77214879e5d2cf3a3659c12fa5eba8
ryu1000 <jccava09@gmail.com>
2018-07-15 18:21:36 -0600

    initial commit

---
+++ BullCowGame/FBullCowGame.cpp
@@ -0,0 +1,58 @@
+#include "FBullCowGame.h"
+
+using int32 = int;
+
+FBullCowGame::FBullCowGame() { Reset(); }
+
+int32 FBullCowGame::GetMaxTries() const { return MyMaxTries;}
+int32 FBullCowGame::GetCurrentTry() const { return MyCurrentTry; }
+int32 FBullCowGame::GetHiddenWordLength() const { return MyHiddenWord.length(); }
+
+void FBullCowGame::Reset()
+{
+	constexpr int32 MAX_TRIES = 8;
+	const FString HIDDEN_WORD = "planet";
+
+	MyMaxTries = MAX_TRIES;
+	MyHiddenWord = HIDDEN_WORD;
+	MyCurrentTry = 1;
+	return;
+}
+
+bool FBullCowGame::IsGameWon() const
+{
+	return false;
+}
+
+EWordStatus FBullCowGame::CheckGuessValidity(FString) const
+{
+	return EWordStatus::OK; // TODO make actual error
+}
+
+// receives a VALID guess, incriments turn, and returns count
+FBullCowCount FBullCowGame::SubmitGuess(FString Guess)
+{
+	// incriment the turn number
+	MyCurrentTry++;
+
+	// setup a return variable
+	FBullCowCount BullCowCount;
+	
+	// loop through all letters in the guess
+	int32 HiddenWordLength = MyHiddenWord.length();
+	for (int32 MHWChar = 0; MHWChar < HiddenWordLength; MHWChar++) {
+		// compare letters against the hidden word
+		for (int32 GChar = 0; GChar < HiddenWordLength; GChar++) {
+			// if they match then
+			if (Guess[GChar] == MyHiddenWord[MHWChar]) {
+				if (MHWChar == GChar) { // if they're in the same place
+					BullCowCount.Bulls++; // incriment bulls
+				}
+				else {
+					BullCowCount.Cows++; // must be a cow
+				}
+			}
+		}
+	}
+	return BullCowCount;
+}
---
+++ BullCowGame/FBullCowGame.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <string>
+
+using FString = std::string;
+using int32 = int;
+
+// all values intialised to zero
+struct FBullCowCount
+{
+	int32 Bulls = 0;
+	int32 Cows = 0;
+};
+
+
+enum class EWordStatus
+{
+	OK,
+	Not_Isogram,
+	Wrong_Length,
+	Not_Lowercase
+};
+
+
+class FBullCowGame
+{
+public:
+	FBullCowGame(); // constructor
+
+	int32 GetMaxTries() const;
+	int32 GetCurrentTry() const;
+	int32 GetHiddenWordLength() const;
+
+	bool IsGameWon() const;
+	EWordStatus CheckGuessValidity(FString) const;
+
+	void Reset(); // TODO make a more rich return value.
+	FBullCowCount SubmitGuess(FString);
+
+// ^^ Please try and ignore this and focus on the interface above ^^
+private:
+	// see constructor for initialisation
+	int32 MyCurrentTry;
+	int32 MyMaxTries;
+	FString MyHiddenWord;
+};
